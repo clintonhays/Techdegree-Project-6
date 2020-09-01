505cd49bf84eaad82ce109bf8d24c5bf00ae7079
clintonhays <clintonhays84@gmail.com>
2020-09-01 16:12:39 +0900

    save progress

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 //
 
 const overlay = document.getElementById('overlay');
-const startGame = document.getElementsByClassName('btn__reset')[0];
-const keys = document.getElementById('qwerty');
+const restartGame = document.getElementsByClassName('btn__reset')[0];
+const keyrows = document.getElementById('qwerty');
+const keys = document.getElementsByTagName('button');
 const phrase = document.getElementById('phrase');
+const phraseUl = phrase.firstElementChild;
 const hearts = document.getElementsByClassName('tries');
 const heartTotal = document.getElementById('scoreboard').firstElementChild;
 let missed = 0;
@@ -40,11 +42,12 @@ function getRandPhraseAsArray (arr) {
 	return chars;
 }
 
+// add random phrase to display
 function addPhraseToDisplay (arr) {
 	for (let i = 0; i < arr.length; i++) {
 		let li = document.createElement('li');
 		li.textContent = arr[i];
-		phrase.append(li);
+		phraseUl.append(li);
 		if (li.textContent !== ' ') {
 			li.className = 'letter';
 		}
@@ -55,6 +58,10 @@ function addPhraseToDisplay (arr) {
 }
 
 // How to refactor this code?
+// checks win condition by comparing shown letters
+// to the length of the phrase array
+// checks loss condition by comparin # of missed guesses
+// to maximum lives
 function checkWin () {
 	const letters = document.getElementsByClassName('letter').length;
 	const shown = document.getElementsByClassName('show');
@@ -64,8 +71,8 @@ function checkWin () {
 			overlay.className = 'win';
 			overlay.firstElementChild.textContent = 'Condragulations!';
 			overlay.style.display = 'flex';
-			startGame.textContent = 'One more time';
-			subtitle.textContent = " You're a winner, baby!";
+			restartGame.textContent = 'One more time';
+			subtitle.textContent = "You're a winner, baby!";
 		}, 2000);
 	}
 	else if (missed >= 5) {
@@ -73,10 +80,32 @@ function checkWin () {
 			overlay.className = 'lose';
 			overlay.firstElementChild.textContent = 'Sashay away...';
 			overlay.style.display = 'flex';
-			startGame.textContent = 'One more time';
+			restartGame.textContent = 'One more time';
 			subtitle.textContent = `Answer: ${phraseArray.join('')}`;
 		}, 200);
 	}
+}
+
+// resets hearts, currently not working
+function resetHearts () {
+	const hidden = document.querySelectorAll('.hidden');
+	for (let i = 0; i < hidden.length; i++) {
+		hidden[i].classList.remove('hidden');
+	}
+}
+
+// resests chosen keys on keyboard
+function resetGuesses () {
+	const chosen = document.querySelectorAll('.chosen');
+	for (let i = 0; i < chosen.length; i++) {
+		chosen[i].style.backgroundColor = '';
+		chosen[i].classList.remove('chosen');
+	}
+}
+
+// resets phrases, currently not working
+function resetPhrase () {
+	phraseUl.innerHTML = '';
 }
 
 //
@@ -88,13 +117,23 @@ function checkWin () {
 //
 
 // button click sets initial overlay display to none
-startGame.addEventListener('click', (e) => {
-	overlay.style.display = 'none';
+restartGame.addEventListener('click', (e) => {
+	resetGame();
+	// overlay.style.display = 'none';
 });
 
-addPhraseToDisplay(phraseArray);
+// addPhraseToDisplay(phraseArray);
 
-qwerty.addEventListener('click', (e) => {
+function resetGame () {
+	missed = 0;
+	resetPhrase();
+	addPhraseToDisplay(phraseArray);
+	resetGuesses();
+	resetHearts();
+	overlay.style.display = 'none';
+}
+
+keyrows.addEventListener('click', (e) => {
 	const button = e.target;
 
 	function checkLetter (button) {
@@ -102,7 +141,6 @@ qwerty.addEventListener('click', (e) => {
 		let match = null;
 		for (let i = 0; i < letters.length; i++) {
 			if (button.textContent.toLowerCase() === letters[i].textContent.toLowerCase()) {
-				// letters[i].className = 'show'; // This line does not add class name to consecutive repeated letters. Why?
 				letters[i].classList.add('show');
 				button.style.backgroundColor = '#6DB0C5';
 				match += button.textContent;
@@ -117,7 +155,7 @@ qwerty.addEventListener('click', (e) => {
 		if (letterFound === null) {
 			button.style.backgroundColor = '#FF70A6';
 			for (let i = 0; i < hearts.length; i++) {
-				heartTotal.removeChild(hearts[i]);
+				hearts[i].className = 'hidden';
 				break;
 			}
 			missed++;
