4b81ec0adfaa35f6b2aa7729863c3acc7c4f1ea9
clintonhays <clintonhays84@gmail.com>
2020-09-02 13:17:17 +0900

    recfactor, style changes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//
-// - - - - - Global Variables - - - - -
-//
+//										//
+// - - - - - Global Variables - - - - - //
+//										//
 
 const overlay = document.getElementById('overlay');
 const startGame = document.getElementsByClassName('btn__start')[0];
-const restartGame = document.getElementsByClassName('btn__reset')[0];
-const keyrows = document.getElementById('qwerty');
-const keys = document.getElementsByTagName('button');
 const phrase = document.getElementById('phrase');
 const phraseUl = phrase.firstElementChild;
-const hearts = document.getElementsByClassName('tries');
+const keyrows = document.getElementById('qwerty');
+const keys = document.getElementsByTagName('button');
 const heartTotal = document.getElementById('scoreboard').firstElementChild;
-const removedHearts = [];
+const hearts = document.getElementsByClassName('tries');
 let missed = 0;
+const removedHearts = [];
+const restartGame = document.getElementsByClassName('btn__reset')[0];
 
 const phrases = [
 	'Shantay you stay',
@@ -20,21 +20,20 @@ const phrases = [
 	'Reading is fundamental',
 	'She done already done had herses',
 	'Can I get an amen',
-	'Oh no she better dont'
+	'Oh no she bettah dont'
 ];
 
 let phraseArray = getRandPhraseAsArray(phrases);
 
-//
-// - - - - - End Global Variables - - - - -
-//
+//											//
+// - - - - - End Global Variables - - - - - //
+//											//
 
-//
-// - - - - - Global Functions - - - - -
-//
+//										//
+// - - - - - Global Functions - - - - - //
+//										//
 
-// get random phrase from [phrases] and return an
-// array of the characters from the phrase
+// get random phrase from [phrases] and return an array of the characters from the phrase //
 function getRandPhraseAsArray (arr) {
 	const random = Math.floor(Math.random() * arr.length);
 	let chars = [];
@@ -44,7 +43,7 @@ function getRandPhraseAsArray (arr) {
 	return chars;
 }
 
-// add random phrase to display
+// add random phrase to display //
 function addPhraseToDisplay (arr) {
 	for (let i = 0; i < arr.length; i++) {
 		let li = document.createElement('li');
@@ -59,42 +58,73 @@ function addPhraseToDisplay (arr) {
 	}
 }
 
-// How to refactor this code?
-// checks win condition by comparing shown letters
-// to the length of the phrase array
-// checks loss condition by comparing # of missed guesses
-// to maximum lives
+// check target text content agianst phraseArray indices //
+function checkLetter (target) {
+	const letters = document.getElementsByClassName('letter');
+	let match = null;
+	for (let i = 0; i < letters.length; i++) {
+		if (target.textContent.toLowerCase() === letters[i].textContent.toLowerCase()) {
+			letters[i].classList.add('show');
+			target.style.backgroundColor = '#6DB0C5';
+			match += target.textContent;
+		}
+	}
+	if (target.tagName === 'BUTTON' && target.className !== 'chosen') {
+		target.className = 'chosen';
+		const letterFound = checkLetter(target);
+		if (letterFound === null) {
+			target.style.backgroundColor = '#FF70A6';
+			for (let i = 0; i < hearts.length; i++) {
+				removedHearts.push(heartTotal.removeChild(hearts[i]));
+				break;
+			}
+			missed++;
+		}
+	}
+	return match;
+}
+
+// checks win condition by comparing shown letters to the length of the phrase array //
+// checks loss condition by comparing # of missed guesses to length of <ol> //
 function checkWin () {
 	const letters = document.getElementsByClassName('letter').length;
 	const shown = document.getElementsByClassName('show');
 	const subtitle = document.getElementsByClassName('subtitle')[0];
-	if (letters === shown.length) {
-		setTimeout(() => {
-			overlay.className = 'win';
-			overlay.firstElementChild.textContent = 'Condragulations!';
+	const endScreen = {
+		base : () => {
 			overlay.style.display = 'flex';
 			restartGame.style.display = 'inline';
 			restartGame.textContent = 'One more time';
 			startGame.textContent = 'Quit';
 			startGame.setAttribute('href', 'index.html');
+		},
+		win  : () => {
+			overlay.className = 'win';
+			overlay.firstElementChild.textContent = 'Condragulations!';
 			subtitle.textContent = "You're a winner, baby!";
-		}, 1000);
+		},
+		lose : () => {
+			overlay.className = 'lose';
+			overlay.firstElementChild.textContent = 'Sashay away...';
+			subtitle.textContent = `Answer: ${phraseArray.join('')}`;
+		}
+	};
+
+	if (letters === shown.length) {
+		setTimeout(() => {
+			endScreen.base();
+			endScreen.win();
+		}, 1800);
 	}
 	else if (missed >= 5) {
 		setTimeout(() => {
-			overlay.className = 'lose';
-			overlay.firstElementChild.textContent = 'Sashay away...';
-			overlay.style.display = 'flex';
-			restartGame.style.display = 'inline';
-			restartGame.textContent = 'One more time';
-			startGame.textContent = 'Quit';
-			startGame.setAttribute('href', 'index.html');
-			subtitle.textContent = `Answer: ${phraseArray.join('')}`;
+			endScreen.base();
+			endScreen.lose();
 		}, 200);
 	}
 }
 
-// resets hearts
+// resets hearts //
 function resetHearts () {
 	missed = 0;
 	for (let i = 0; i < removedHearts.length; i++) {
@@ -102,7 +132,7 @@ function resetHearts () {
 	}
 }
 
-// resests chosen keys on keyboard
+// resests chosen keys on keyboard //
 function resetGuesses () {
 	const chosen = document.querySelectorAll('.chosen');
 	for (let i = 0; i < chosen.length; i++) {
@@ -111,31 +141,29 @@ function resetGuesses () {
 	}
 }
 
-// resets phrases, currently not working
-// the same phrase is returned when game is restarted
+// resets phrases //
 function resetPhrase () {
 	phraseUl.innerHTML = '';
 	addPhraseToDisplay(getRandPhraseAsArray(phrases));
 }
 
-// resets game
+// resets game //
 function resetGame () {
-	missed = 0;
 	resetPhrase();
 	resetGuesses();
 	resetHearts();
 	overlay.style.display = 'none';
 }
 
-//
-// - - - - - End Global Functions - - - - -
-//
+//											//
+// - - - - - End Global Functions - - - - - //
+//											//
 
-//
-// - - - - - Initialize Game - - - - -
-//
+//									   //
+// - - - - - Initialize Game - - - - - //
+//									   //
 
-// button click sets initial overlay display to none
+// button click sets initial overlay display to none //
 startGame.addEventListener('click', (e) => {
 	addPhraseToDisplay(phraseArray);
 	overlay.style.display = 'none';
@@ -143,33 +171,7 @@ startGame.addEventListener('click', (e) => {
 
 keyrows.addEventListener('click', (e) => {
 	const button = e.target;
-
-	function checkLetter (button) {
-		const letters = document.getElementsByClassName('letter');
-		let match = null;
-		for (let i = 0; i < letters.length; i++) {
-			if (button.textContent.toLowerCase() === letters[i].textContent.toLowerCase()) {
-				letters[i].classList.add('show');
-				button.style.backgroundColor = '#6DB0C5';
-				match += button.textContent;
-			}
-		}
-		return match;
-	}
-
-	if (button.tagName === 'BUTTON' && button.className !== 'chosen') {
-		button.className = 'chosen';
-		const letterFound = checkLetter(button);
-		if (letterFound === null) {
-			button.style.backgroundColor = '#FF70A6';
-			for (let i = 0; i < hearts.length; i++) {
-				removedHearts.push(heartTotal.removeChild(hearts[i]));
-				console.log(removedHearts);
-				break;
-			}
-			missed++;
-		}
-	}
+	checkLetter(button);
 	checkWin();
 });
 
